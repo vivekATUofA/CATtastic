31fd09fa5dabf8a91f8c65b3c64b9ff6b060f881
vivekaswal <vivekaswal@arizona.edu>
2025-03-01 01:57:26 -0500

    Intial commit

---
+++ src/test/java/com/uofa/CATtastic/CaTtasticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uofa.CATtastic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaTtasticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
